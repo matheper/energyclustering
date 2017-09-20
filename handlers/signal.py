9afba1506c4f54b426dfa951deca2e14cce89994
Matheus Pereira <matheper@gmail.com>
2017-09-20 01:06:45 -0300

    Added post method to receive energy signal.

--- handlers/signal.py
+++ handlers/signal.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 import tornado
 
+from utils import parseSignal
+
+from models import Signal
+from models import Device
+
 
 class SignalHandler(tornado.web.RequestHandler):
 
-    def get(self):
-        print('get')
+    def initialize(self, session):
+        self.session = session()
+
+    def post(self):
+        try:
+            signal_data = parseSignal(self.request.body.decode())
+            device = Device(signal_data.get('Device'))
+            if not self.session.query(Device).filter_by(id=device.id).first():
+                self.session.add(device)
+                self.session.commit()
+            signal_data['Device ID'] = device.id
+            signal = Signal(signal_data)
+            self.session.add(signal)
+            self.session.commit()
+        except:
+            msg = "Could not decode: %s" % self.request.body
+            raise tornado.web.HTTPError(400, msg)
--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from tornado.web import URLSpec as url
 
+import app
 from handlers.signal import SignalHandler
 
 
 url_handlers = [
-    url(r'/signal', SignalHandler),
+    url(r'/signal', SignalHandler, dict(
+            session=app.db_session,
+    )),
 ]
