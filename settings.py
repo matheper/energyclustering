90391fbf5b43c78644fa2090b7ac283d7b66f237
Matheus Pereira <matheper@gmail.com>
2017-09-20 15:57:05 -0300

    Added postgresql via docker-compose.

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from tornado.options import define
 from tornado.options import options
 
 define("port", default=8888, help="run on the given port", type=int)
-define("pg_host", default="127.0.0.1:5432", help="database host")
+define("pg_host", default="127.0.0.1:3306", help="database host")
 define("pg_database", default="ecluster", help="database name")
 define("pg_user", default="postgres", help="database user")
 define("pg_password", default="postgres", help="database password")
